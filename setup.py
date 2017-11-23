d8892bdc062b80c1449c98ed30e2cc0d28b9bf55
Helena Nawrath <h.nawrath@yahoo.de>
2017-11-23 11:13:07 +0100

    add package structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='all_sky_cloud_detection',
+    author='Helena Nawrath',
+    author_email='helena.nawrath@tu-dortmund.de',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-image',
+        'astropy>=2',
+
+    ]
+)
