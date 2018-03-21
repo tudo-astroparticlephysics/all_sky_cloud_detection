4381140d16378e778f783a8b56e15d8a37d6fc6e
Helena Nawrath <h.nawrath@yahoo.de>
2018-03-21 09:35:39 +0100

    added new files

---
+++ all_sky_cloud_detection/celestial_objects.py
@@ -0,0 +1,75 @@
+from astropy.coordinates import solar_system_ephemeris, SkyCoord, AltAz
+from all_sky_cloud_detection.coordinate_transformation import spherical2pixel
+import numpy as np
+from astropy.coordinates import get_body
+
+def get_planets_altaz(time, cam):
+    """This function searches for positions of celestial objects at a given time
+    Parameters
+    -----------
+    time: astropy SkyCoord time object
+            Time from the fits header of the image
+    cam: string
+        Camera name
+    Returns
+    -------
+    pos_altaz: array
+        Array of celestial objects in altaz
+    """
+    observer = cam.location
+    with solar_system_ephemeris.set('builtin'):
+        moon = get_body('moon', time, observer)
+        sun = get_body('sun', time, observer)
+        mercury = get_body('mercury', time, observer)
+        venus = get_body('venus', time, observer)
+        mars = get_body('mars', time, observer)
+        jupiter = get_body('jupiter', time, observer)
+        saturn = get_body('saturn', time, observer)
+        uranus = get_body('uranus', time, observer)
+        neptune = get_body('neptune', time, observer)
+
+    ra_objects = [
+        moon.ra,
+        sun.ra,
+        mercury.ra,
+        venus.ra,
+        mars.ra,
+        jupiter.ra,
+        saturn.ra,
+        uranus.ra,
+        neptune.ra]
+    dec_objects = [
+        moon.dec,
+        sun.dec,
+        mercury.dec,
+        venus.dec,
+        mars.dec,
+        jupiter.dec,
+        saturn.dec,
+        uranus.dec,
+        neptune.dec]
+    pos = SkyCoord(ra=ra_objects, dec=dec_objects, frame='icrs', unit='deg')
+    pos_altaz = pos.transform_to(AltAz(obstime=time, location=observer))
+    return pos_altaz
+
+
+def get_planets(time, cam):
+    """This function searches for positions of celestial objects at a given time
+    Parameters
+    -----------
+    time: astropy SkyCoord time object
+            Time from the fits header of the image
+    cam: string
+        Camera name
+    Returns
+    -------
+    row: Array
+        Pixel coordinates of celestial objects on the y axis
+    col: Array
+        Pixel coordinates of celestial objects on the x axis
+    """
+    pos_altaz = get_planets_altaz(time, cam)
+    row, col = spherical2pixel(pos_altaz.alt, pos_altaz.az, cam.lens.theta2r, cam)
+    row, col = row[0], col[0]
+    size = np.ones(len(row))
+    return row, col, size
---
+++ all_sky_cloud_detection/find_blobs.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+from all_sky_cloud_detection.io import read_fits
+from all_sky_cloud_detection.preparation import normalize_image
+from all_sky_cloud_detection.star_detection import find_stars
+
+
+def find_blobs(img_name, file_type, threshold, overlap):
+    """This function searches for bright blobs above the threshold in an image.
+
+    Parameters
+     -----------
+    img_name : string
+        Name of the processed image.
+    format : string
+        Format of the image.
+    threshold : float
+        Reduce this to detect blobs with less intensities.
+
+    Returns
+    -------
+    row : array
+        y coordinates of found blobs in pixels
+    col : array
+        x coordinates of found blobs in pixles
+    size: array
+        Standard deviation of the Gaussian kernel which detected the blob.
+    image: pdf file
+        Image with blobs saved in same directory as the input file
+    """
+
+    if file_type == 'fits':
+        scale = 2**16
+        image = normalize_image(read_fits(img_name), scale=scale)
+        row, col, size = find_stars(image, threshold=threshold, overlap=overlap)
+        return row, col, size
+
+    #improve else case when more file types are supported
+    else:
+        scale = 2**16
+        image = normalize_image(read_fits(img_name), scale=scale)
+        row, col, size = find_stars(image, threshold=threshold)
+        return row, col, size
