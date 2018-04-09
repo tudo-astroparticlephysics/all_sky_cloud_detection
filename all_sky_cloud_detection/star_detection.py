8fec95f4970a4a41a094feaa1a3610b2d115261d
Helena Nawrath <h.nawrath@yahoo.de>
2018-04-09 11:16:26 +0200

    added comments

--- all_sky_cloud_detection/star_detection.py
+++ all_sky_cloud_detection/star_detection.py
@@ -3,6 +3,22 @@ import warnings
 
 
 def find_stars(image, threshold):
+    """This function finds bright blobs in an image above a given threshold.
+    Parameters
+    -----------
+    image: array
+            Image, normalized with the normalize_image() function
+    threshold: float
+                Determines wheter more or less bright blobs are found
+    Returns
+    -------
+    row: array
+        pixel positions of found blobs (y axis)
+    col: array
+        pixel postions of found blobs (x axis)
+    size: array
+            size of the found blobs in pixels
+    """
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore')
         blobs = blob_log(
