68fd34b341bd775a8987e36bc3f3fd96833bc56e
Helena Nawrath <h.nawrath@yahoo.de>
2018-04-09 11:15:44 +0200

    added comments

--- all_sky_cloud_detection/preparation.py
+++ all_sky_cloud_detection/preparation.py
@@ -2,5 +2,17 @@ from skimage import img_as_float
 
 
 def normalize_image(image, scale=1.0):
+    """This function normalizes an image.
+    Parameters
+    -----------
+    image: path
+            Path where the image is located
+    scale: float
+            Parameter, depending on the image
+    Returns
+    -------
+    array
+    Normalized image
+    """
     image = img_as_float(image)
     return image / scale
