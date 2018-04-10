2d674f7f18e59af0b6b90f1f737bf19024f408d9
Helena Nawrath <h.nawrath@yahoo.de>
2018-04-10 11:02:46 +0200

    support matlab files

--- all_sky_cloud_detection/io.py
+++ all_sky_cloud_detection/io.py
@@ -1,7 +1,14 @@
 from astropy.io import fits
+import scipy.io as sio
 
 
 def read_fits(path):
     with fits.open(path) as f:
         image = f[0].data
     return image
+
+
+def read_matlab(path):
+	image = sio.loadmat(path)
+	image = image['pic1']
+	return image
