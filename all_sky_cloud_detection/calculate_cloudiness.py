3d2be772b58295448ded8c0da339fa1292bfac42
Helena Nawrath <h.nawrath@yahoo.de>
2018-04-16 12:23:23 +0200

    corrected input of cl

--- all_sky_cloud_detection/calculate_cloudiness.py
+++ all_sky_cloud_detection/calculate_cloudiness.py
@@ -8,10 +8,12 @@ def calculate_cloudiness(cam, catalog, matches):
     resolution_col = cam.sensor.resolution_col
 
     row, col = spherical2pixel(catalog.alt, catalog.az, cam.lens.theta2r, cam)
-
+    row = row[0]
+    col = col[0]
     row_reduced = row[((row) - (resolution_row/2))**2 + (col - (resolution_col/2))**2 < (245-25 / 2)**2]
     col_reduced = col[((row) - (resolution_row/2))**2 + (col - (resolution_col/2))**2 < (245-25 / 2)**2]
     size = np.ones(len(col_reduced))
-
+    # row_red, col_red, size nur zum plotten wichtig
+    # länge der arrays nur wichtig für bewölkung
     cl = np.round(1-matches/len(row_reduced), 2)
     return cl
