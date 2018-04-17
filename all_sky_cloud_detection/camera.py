78c708592b3d0299a8b1e5b8c8bc91e3e36ae862
Helena Nawrath <h.nawrath@yahoo.de>
2018-04-17 13:02:51 +0200

    Delete camera.py

--- all_sky_cloud_detection/camera.py
+++
@@ -1,9 +0,0 @@
-
-from all_sky_cloud_detection import camera_classes as camclass
-
-
-def camera(cam):
-    if cam == 'cta':
-        return camclass.cta
-    if cam == 'iceact':
-        return camclass.iceact
