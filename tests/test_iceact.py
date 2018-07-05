11d0d11628e93a34eff17c86eb6f317d0943a852
Helena Nawrath <h.nawrath@yahoo.de>
2018-07-05 11:59:20 +0200

    added example and test for a starry image from the south pole

--- tests/test_iceact.py
+++ tests/test_iceact.py
@@ -1,9 +1,17 @@
 
 
-def test():
+def test_cloudy():
     from all_sky_cloud_detection.process_image import process_image
     from all_sky_cloud_detection.camera_classes import iceact
 
     cl, time, mean_brightness = process_image('tests/resources/iceact_images/cloudy.fits', 'fits', iceact)
 
     assert cl > 0.75
+
+def test_starry():
+    from all_sky_cloud_detection.process_image import process_image
+    from all_sky_cloud_detection.camera_classes import iceact
+
+    cl, time, mean_brightness = process_image('tests/resources/iceact_images/starry.fits', 'fits', iceact)
+
+    assert cl < 0.2
