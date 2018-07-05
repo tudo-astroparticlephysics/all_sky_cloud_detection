9557b9711cebb2febbe154cb553cd4c08193e8b6
Helena Nawrath <h.nawrath@yahoo.de>
2018-07-05 10:25:14 +0200

    example for processing iceact images

--- examples/process_iceact.py
+++ examples/process_iceact.py
@@ -1,6 +1,26 @@
 from all_sky_cloud_detection.process_image import process_image
 import pandas as pd
-import all_sky_cloud_detection.camera_classes as camclass
-cl, time = process_image('../tests/resources/iceact/06/0613/0000.fits', 'fits', camclass.iceact)
-df = pd.DataFrame({'cloudiness': cl, 'time': time})
-cloudiness = df.to_csv('test.csv')
+
+from all_sky_cloud_detection.camera_classes import iceact
+
+
+images = [
+    '../tests/resources/iceact_images/cloudy.fits'
+]
+
+results = []
+
+for img in images:
+    cl, time, mean_brightness = process_image(img, cam=iceact, file_format='fits')
+
+    results.append({
+        'cloudiness': cl,
+        'timestamp': time.iso,
+        'mean_brightness': mean_brightness,
+        'image': img,
+    })
+
+df = pd.DataFrame(results)
+
+
+df.to_csv('cloudiness.csv')
