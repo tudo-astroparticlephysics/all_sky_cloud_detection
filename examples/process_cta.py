d463ac4086bc61508cbbf1a00011b243e3bcfa9b
Helena Nawrath <h.nawrath@yahoo.de>
2018-07-17 11:34:03 +0200

    fix image path

--- examples/process_cta.py
+++ examples/process_cta.py
@@ -4,4 +4,4 @@ from all_sky_cloud_detection.camera_classes import cta
 
 df = process_images('../../net/big-tank/POOL/projects/cta/all_sky_camera_images/la_palma/2015/*', cta)
 
-df.to_csv('../..cd ~/net/big-tank/POOL/users/hnawrath/results/la_palma_2015.csv')
+df.to_csv('../../net/big-tank/POOL/users/hnawrath/results/la_palma_2015.csv')
