c31f4aea7b9754c8b544a28111c0ce7bad7749c4
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-09-13 11:28:17 +0200

    Adapt tests to new version

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,15 +1,25 @@
+import numpy as np
+
+
 def test_read():
-
     from all_sky_cloud_detection.catalog import read_catalog
-    catalog = read_catalog()
 
-    assert len(catalog) == 118218
+    catalog = read_catalog()
     assert catalog.colnames == ['HIP', 'ra', 'dec', 'v_mag', 'variability']
 
 
 def test_max_magnitude():
     from all_sky_cloud_detection.catalog import read_catalog
 
-    catalog = read_catalog(max_magnitude=8)
+    catalog = read_catalog(max_magnitude=6)
+    assert np.all(catalog['v_mag'] <= 6)
 
-    assert len(catalog) == 2895
+    catalog = read_catalog(max_magnitude=3)
+    assert np.all(catalog['v_mag'] <= 3)
+
+def test_max_variability():
+    from all_sky_cloud_detection.catalog import read_catalog
+
+    catalog = read_catalog(max_variability=2)
+    assert np.any(catalog['variability'] >= 1)
+    assert np.all(catalog['variability'] <= 2)
--- tests/test_cta.py
+++ tests/test_cta.py
@@ -1,31 +1,58 @@
+import numpy as np
+import astropy.units as u
+
+
 def test_read_fits():
     from all_sky_cloud_detection.cameras import CTA
 
     img = CTA.read('tests/resources/cta_images/starry.fits.gz')
 
     assert img.data.shape == (1699, 1699)
-
-def test_cloudy():
-    from all_sky_cloud_detection.process_image import process_image
-    from all_sky_cloud_detection.camera_classes import cta
-
-    cl, time, mean_brightness, moon_altitude = process_image('tests/resources/cta_images/cloudy.fits.gz', cta)
-
-    assert cl > 0.75
-
-def test_starry():
-    from all_sky_cloud_detection.process_image import process_image
-    from all_sky_cloud_detection.camera_classes import cta
-
-    cl, time, mean_brightness, moon_altitude = process_image('tests/resources/cta_images/starry.fits.gz', cta)
-
-    assert cl < 0.2
+    assert np.all(img.data >= 0)
+    assert np.all(img.data <= 1)
 
 
-def test_starry():
-    from all_sky_cloud_detection.process_image import process_image
-    from all_sky_cloud_detection.camera_classes import cta
+def test_images():
+    from all_sky_cloud_detection.cameras import cta_la_palma as cam
+    from all_sky_cloud_detection.catalog import read_catalog, transform_catalog
+    from all_sky_cloud_detection.star_detection import find_stars, find_matching_stars
+    from all_sky_cloud_detection.calculate_cloudiness import calculate_cloudiness_simple
 
-    cl, time, mean_brightness, moon_altitude = process_image('tests/resources/cta_images/partly_cloudy.fits.gz', cta)
+    images = [
+        'tests/resources/cta_images/starry_nomoon.fits.gz',
+        'tests/resources/cta_images/starry.fits.gz',
+        'tests/resources/cta_images/partly_cloudy.fits.gz',
+        'tests/resources/cta_images/cloudy.fits.gz',
+    ]
 
-    assert cl > 0.25 and cl < 0.75
+    limits = [
+        (0, 0.1),
+        (0, 0.2),
+        (0.3, 0.7),
+        (0.9, 1.0),
+    ]
+
+    catalog = read_catalog(
+        max_magnitude=cam.max_magnitude,
+        max_variability=None,
+    )
+
+    for (a, b), path in zip(limits, images):
+        img = cam.read(path)
+
+        # create catalog of possibly visible stars and planets
+        stars_catalog, magnitude = transform_catalog(
+            catalog, img.timestamp, cam, min_altitude=20,
+        )
+
+        r, c, s = find_stars(img.data, threshold=cam.threshold)
+        found_altaz = cam.pixel2horizontal(r, c, img.timestamp)
+        mask = found_altaz.alt.deg > 20
+
+        idx, found = find_matching_stars(
+            stars_catalog, found_altaz[mask], max_sep=0.25 * u.deg
+        )
+
+        cl = calculate_cloudiness_simple(found)
+
+        assert a <= cl <= b
