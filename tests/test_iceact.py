7c6231e23d8fb87dac31f514d9bbc603ace666ab
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-09-13 12:31:44 +0200

    Adapt tests

--- tests/test_iceact.py
+++ tests/test_iceact.py
@@ -1,17 +1,43 @@
+import astropy.units as u
 
 
-def test_cloudy():
-    from all_sky_cloud_detection.process_image import process_image
-    from all_sky_cloud_detection.camera_classes import iceact
+def test_images():
+    from all_sky_cloud_detection.cameras import iceact as cam
+    from all_sky_cloud_detection.catalog import read_catalog, transform_catalog
+    from all_sky_cloud_detection.star_detection import find_stars, find_matching_stars
+    from all_sky_cloud_detection.calculate_cloudiness import calculate_cloudiness_simple
 
-    cl, time, mean_brightness, moon_altitude = process_image('tests/resources/iceact_images/cloudy.fits', iceact)
+    images = [
+        'tests/resources/iceact_images/cloudy.fits',
+        'tests/resources/iceact_images/starry.fits',
+    ]
 
-    assert cl > 0.75
+    limits = [
+        (0.5, 1),
+        (0.0, 0.5),
+    ]
 
-def test_starry():
-    from all_sky_cloud_detection.process_image import process_image
-    from all_sky_cloud_detection.camera_classes import iceact
+    catalog = read_catalog(
+        max_magnitude=cam.max_magnitude,
+        max_variability=None,
+    )
 
-    cl, time, mean_brightness, moon_altitude = process_image('tests/resources/iceact_images/starry.fits', iceact)
+    for (a, b), path in zip(limits, images):
+        img = cam.read(path)
 
-    assert cl < 0.8
+        # create catalog of possibly visible stars and planets
+        stars_catalog, magnitude = transform_catalog(
+            catalog, img.timestamp, cam, min_altitude=20,
+        )
+
+        r, c, s = find_stars(img.data, threshold=cam.threshold)
+        found_altaz = cam.pixel2horizontal(r, c, img.timestamp)
+        mask = found_altaz.alt.deg > 20
+
+        idx, found = find_matching_stars(
+            stars_catalog, found_altaz[mask], max_sep=0.25 * u.deg
+        )
+
+        cl = calculate_cloudiness_simple(found)
+
+        assert a <= cl <= b
